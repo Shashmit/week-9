c9c42ea55103aacc8dd758a097ff336f320ba2ed
Shashmit kumar <shaashmit@gmail.com>
2023-11-28 22:08:55 +0530

    deploy

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,2 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "" ubuntu@ec2-54-91-238-249.compute-1.amazonaws.com "sudo bash ~/deploy.sh"
+ssh -t -i EC2\ instance\ key.pem ubuntu@ec2-54-91-238-249.compute-1.amazonaws.com "source deploy.sh"
